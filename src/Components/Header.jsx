508480d7b0370af820ab10ae96468f8b615d8d98
shresthanishcom <me.anish.shrestha@gmail.com>
2022-01-04 13:07:55 +0545

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,22 @@
+import react from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src="./pictures/logo.png" alt="microsoft logo" />
+      </div>
+      <div className="links">
+        <ul>
+          <li>Office</li>
+          <li>Windows</li>
+          <li>Surface</li>
+          <li>Xbox</li>
+          <li>Deals</li>
+          <li>Support</li>
+        </ul>
+      </div>
+      <div className="navigation">something</div>
+    </div>
+  );
+}
